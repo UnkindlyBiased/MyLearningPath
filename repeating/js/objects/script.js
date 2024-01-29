8a931b29181c0527e9a0c84c0b4da90ca7007e94
UnkindlyBiased <inoutoftheverge@gmail.com>
2024-01-29 10:54:01 +0200

    JS: function constructors

--- repeating/js/objects/script.js
+++ repeating/js/objects/script.js
@@ -43,4 +43,54 @@ let ladder = {
     }
 }
 
-ladder.up().up().showStep()
+// ladder.up().up().showStep()
+
+function Something(name) {
+    this.name = name,
+    this.isAdmin = false
+}
+
+let someone = new Something("The Who")
+// alert(someone.name)
+
+function ReturnTest(newName) {
+    this.newName = newName
+    return;
+}
+
+let test = new ReturnTest("brutmonster")
+console.log(test)
+
+function CalcFunc() {
+    this.numbers = {
+        a: 0,
+        b: 0
+    }
+    this.read = () => {
+        this.numbers.a = Number(prompt("Введіть число A"))
+        this.numbers.b = Number(prompt("Введіть число B"))
+    }
+    this.sum = () => {
+        return this.numbers.a + this.numbers.b
+    }
+    this.multiply = () => {
+        return this.numbers.a * this.numbers.b
+    }
+}
+
+let newCalc = new CalcFunc
+// newCalc.read()
+// alert(newCalc.sum())
+
+function Accumulator(startValue) {
+    this.startValue = startValue
+    this.read = () => {
+        this.startValue += Number(prompt("Введіть значення"))
+        alert(this.startValue)
+    }
+}
+
+let accum = new Accumulator(12)
+document.getElementById('addValue').addEventListener('click', () => {
+    accum.read()
+})
